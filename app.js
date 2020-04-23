a2f23a8297dbdc57b1e6594084adae47b0a478b4
Zafar Ali <zafar.a.ali2@gmail.com>
2020-04-23 09:54:23 -0400

    Delete test user and add new user in env

--- app.js
+++ app.js
@@ -13,4 +13,5 @@ app.use(express.static('./public'));
 todoController(app);
 
 //listen to port
-app.listen(process.env.PORT || 5000)
+app.listen(3000);
+console.log("You are listening to port 3000");
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,54 +1,48 @@
 var bodyParser = require('body-parser');
-var mongoose= require('mongoose');
-
+var mongoose = require('mongoose');
+require('dotenv').config();
+const NODE_USERNAME = process.env.NODE_USERNAME;
+const NODE_PASSWORD = process.env.NODE_PASSWORD;
 //Connect to database Mlab
-mongoose.connect('mongodb://test:test@ds263948.mlab.com:63948/todo');
+mongoose.connect(
+  'mongodb://' +
+    NODE_USERNAME +
+    ':' +
+    NODE_PASSWORD +
+    '@ds263948.mlab.com:63948/todo'
+);
 
 //Create Schema
 var todoSchema = new mongoose.Schema({
-  item: String
+  item: String,
 });
 
-var Todo = mongoose.model('Todo',todoSchema);
-
-
-//var data=[{item: 'Get Milk'},{item: 'Walk Dog'},{item: 'Cold Shower'}];
-var urlencodedParser=bodyParser.urlencoded({extended: false});
-module.exports = function(app){
-
-app.get('/todo', function(req, res){
-
-  //get data from mongodb and pass it to view
-  Todo.find({}, function(err,data){
-    if (err) throw err;
-res.render('todo',{todos: data});
-
-});
-});
-
-
-app.post('/todo', urlencodedParser,function(req, res){
-
-  //get data from the view and add it to mongodb
-
-var newTodo= Todo(req.body).save(function(err,data){
-  if (err) throw err;
-  res.json(data);
-});
-
-
-
-
-});
-
-
-app.delete('/todo/:item', function(req, res){
-  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
-    if (err) throw err;
-    res.json(data);
+var Todo = mongoose.model('Todo', todoSchema);
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    //get data from mongodb and pass it to view
+    Todo.find({}, function (err, data) {
+      if (err) throw err;
+      res.render('todo', { todos: data });
+    });
   });
 
+  app.post('/', urlencodedParser, function (req, res) {
+    //get data from the view and add it to mongodb
+    var newTodo = Todo(req.body).save(function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
 
-});
-
-}
+  app.delete('/:item', function (req, res) {
+    Todo.find({ item: req.params.item.replace(/\-/g, ' ') }).remove(function (
+      err,
+      data
+    ) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+};
